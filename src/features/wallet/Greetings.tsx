12e18f5b11e05b03b0c04c9cf4ec9873cd8b3e76
Your Name <lamar.lewis@lc.cuny.edu>
2026-06-17 01:04:33 -0400

    removed unused imports

--- src/features/wallet/Greetings.tsx
+++ src/features/wallet/Greetings.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const Greetings = () => {
   return <div>Greetings</div>;
 };
--- src/features/wallet/QuickActions.tsx
+++ src/features/wallet/QuickActions.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const QuickActions = () => {
   return <div>QuickActions</div>;
 };
